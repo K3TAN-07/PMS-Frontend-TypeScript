d2ea7a7772905e88318178c4745e85ae02e73f0b
K3TAN-07 <ketanfunde77@gmail.com>
2023-04-14 14:06:58 +0530

    upgreded kanban 0.1 phase

--- src/@core/components/kanban/kanban.tsx
+++ src/@core/components/kanban/kanban.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { Card } from '@mui/material'
 
@@ -24,7 +24,7 @@ const columns = {
 const KanbanBoard = () => {
   const [tasks, setTasks] = useState(columns)
 
-  const handleOnDragEnd = result => {
+  const handleOnDragEnd = (result: { destination: any; source?: any; draggableId?: any }) => {
     if (!result.destination) return
 
     const { source, destination, draggableId } = result
@@ -46,11 +46,34 @@ const KanbanBoard = () => {
     setTasks(newTasks)
   }
 
-  const handleNewTaskSubmit = (event, columnId) => {
+  const handleNewTaskSubmit = (event: FormEvent<HTMLFormElement>, columnId: string) => {
     event.preventDefault()
     const input = event.target.elements.task
     const newTaskContent = input.value.trim()
     if (!newTaskContent) return
+
+    // check if task already exists in any column
+    const isDuplicateTask = Object.values(tasks).some(column =>
+      column.tasks.some(task => task.content === newTaskContent)
+    )
+    if (isDuplicateTask) {
+      alert('Task already exists in another column')
+      return
+    }
+
+    // check if task exists in requested, inProgress or done columns
+    const isTaskInOtherColumns = ['requested', 'inProgress', 'done']
+      .filter(id => id !== columnId)
+      .some(id => {
+        const column = tasks[id]
+        return column.tasks.some((task: { content: any }) => task.content === newTaskContent)
+      })
+    if (isTaskInOtherColumns) {
+      alert('Task already exists in another column')
+
+      return
+    }
+
     const newTask = { id: `task-${Date.now()}`, content: newTaskContent }
     const column = tasks[columnId]
     const newTasks = { ...tasks, [columnId]: { ...column, tasks: [...column.tasks, newTask] } }
@@ -58,9 +81,9 @@ const KanbanBoard = () => {
     input.value = ''
   }
 
-  const handleTaskDelete = (taskId, columnId) => {
+  const handleTaskDelete = (taskId: never, columnId: string) => {
     const column = tasks[columnId]
-    const filteredTasks = column.tasks.filter(task => task.id !== taskId)
+    const filteredTasks = column.tasks.filter((task: { id: any }) => task.id !== taskId)
     const newTasks = { ...tasks, [columnId]: { ...column, tasks: filteredTasks } }
     setTasks(newTasks)
   }
@@ -74,12 +97,12 @@ const KanbanBoard = () => {
               <h2 className='text-lg font-bold mb-4'>{column.title}</h2>
               <Droppable droppableId={column.id}>
                 {provided => (
-                  <div className='p-2 bg-white rounded shadow-sm' {...provided.droppableProps} ref={provided.innerRef}>
+                  <div className='p-2  rounded shadow-lg' {...provided.droppableProps} ref={provided.innerRef}>
                     {column.tasks.map(({ id, content }, index) => (
                       <Draggable key={id} draggableId={id} index={index}>
                         {provided => (
                           <div
-                            className='mb-2 p-2 bg-gray-100 rounded shadow-sm'
+                            className='mb-2 p-2 bg-violet-300	 border-gray-300   text-black rounded shadow-sm'
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                             ref={provided.innerRef}
@@ -101,7 +124,7 @@ const KanbanBoard = () => {
               </Droppable>
               <form className='mt-4' onSubmit={e => handleNewTaskSubmit(e, column.id)}>
                 <input
-                  className='w-full p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
+                  className='w-full p-2 border border-gray-300  rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
                   type='text'
                   name='task'
                   placeholder='Add a new task...'
